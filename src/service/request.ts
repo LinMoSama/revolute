b0cee71d2bc90e55cc1cdbdebaa807a6106897ce
Crush <835836148@qq.com>
2024-03-13 22:00:19 +0800

    update

--- src/service/request.ts
+++ src/service/request.ts
@@ -6,9 +6,9 @@ let urlPro = 'http://api.revolute.cc'
 let urlDeve = 'http://192.168.2.177:7786'
 // alert(process.env.NODE_ENV);
 const server = axios.create({ 
-  // baseURL: process.env.NODE_ENV === 'development' ? urlDeve : urlPro,
+  baseURL: process.env.NODE_ENV === 'development' ? urlDeve : urlPro,
   // baseURL: 'http://192.168.2.177:7786',
-  baseURL: 'http://api.revolute.cc',
+  // baseURL: 'http://api.revolute.cc',
   timeout: 10000,
 })
 
